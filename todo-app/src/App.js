0db85524dce75ed84fd9bd1e824a4e243da33521
Kinas Maria <kinasmaria01@gmail.com>
2022-05-26 21:38:21 +0300

    LoginPage

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,38 +1,27 @@
-import React, { useState } from 'react';
-import './App.css';
-import ToDoList from './components/ToDoList';
-import Weather from './weather/Weather';
-import Timer from './timer/Timer'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import { LoginPage } from './LoginPage/LoginPage';
+
 
 function App() {
-
-  const [lightTheme, setLightTheme] = useState(false);
-
-  const changeTheme = () => {
-    switch(lightTheme) {
-      case true : {
-        setLightTheme(false);
-        break;
-      } 
-      case false : {
-        setLightTheme(true)
-        break;
-      };
-    }
-  }
-
   return (
-      <div className="app" style={lightTheme?{backgroundColor: 'white'} : {}}>
-        <Weather />
-        <div className="todo-app" style={lightTheme?{backgroundColor: 'grey'} : {}}>
-          <ToDoList Theme={lightTheme}/>
-        </div>
-        <div className="todo-timer">
-          <input onClick={changeTheme} type="checkbox" id="toggle" />
-          <label for="toggle" class="button"></label>
-          <Timer />
-        </div>
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+            path="/"
+            exact element={
+              <MainPage />
+            }
+            />
+        <Route 
+          path="/login"
+          exact element={
+            <LoginPage />
+          }
+          />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
