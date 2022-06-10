5210636919a062b2085528276de7d7f765471c61
Kinas Maria <kinasmaria01@gmail.com>
2022-06-10 10:05:39 +0300

    LoginPart

--- todo-app/src/LoginPage/LoginPage.js
+++ todo-app/src/LoginPage/LoginPage.js
@@ -32,7 +32,7 @@ export const LoginPage = () => {
 	}
 
   return (
-    <div class="s">  	
+    <div class="main">  	
 		<input type="checkbox" id="chk" aria-hidden="true" />
 
 			<div class="signup">
